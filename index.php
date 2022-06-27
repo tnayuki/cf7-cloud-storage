f318234bdb5aff397f4f7e7c389d6f44969e20dd
Toru Nayuki <tnayuki@icloud.com>
2022-06-27 09:58:41 +0900

    First version

---
+++ index.php
@@ -0,0 +1,166 @@
+<?php
+/*
+  Plugin Name: contact-form-7-cloud-storage
+ */
+
+require_once 'vendor/autoload.php';
+
+use Google\Cloud\Storage\StorageClient;
+
+if (is_admin()) {
+  add_action('admin_init', 'cf7_cloud_storage_admin_init');
+  add_action('admin_menu', 'cf7_cloud_storage_admin_menu');
+}
+
+add_action('wp_ajax_get_signed_url', 'cf7_cloud_storage_ajax_get_signed_url');
+add_action('wp_ajax_nopriv_get_signed_url', 'cf7_cloud_storage_ajax_get_signed_url');
+add_action('wpcf7_before_send_mail', 'cf7_cloud_storage_wpcf7_before_send_mail');
+add_action('wpcf7_enqueue_scripts', 'cf7_cloud_storage_wpcf7_enqueue_scripts');
+add_action('wpcf7_init', 'cf7_cloud_storage_wpcf7_init');
+
+function cf7_cloud_storage_admin_init(){
+  register_setting('cf7_cloud_storage', 'cf7_cloud_storage_service_account_key');
+  register_setting('cf7_cloud_storage', 'cf7_cloud_storage_bucket_name_for_uploads');
+  register_setting('cf7_cloud_storage', 'cf7_cloud_storage_bucket_name_for_archives');
+
+  add_settings_section('cf7_cloud_storage_settings_section', 'Google Cloud', null, 'cf7_cloud_storage');
+  add_settings_field(
+    'cf7_cloud_storage_service_account_key',
+    'Service account key',
+    'cf7_cloud_storage_service_account_key_settings_field',
+    'cf7_cloud_storage',
+    'cf7_cloud_storage_settings_section'
+  );
+  add_settings_field(
+    'cf7_cloud_storage_bucket_name_for_uploads',
+    'Bucket name for uploads',
+    'cf7_cloud_storage_bucket_name_for_uploads_settings_field',
+    'cf7_cloud_storage',
+    'cf7_cloud_storage_settings_section'
+  );
+  add_settings_field(
+    'cf7_cloud_storage_bucket_name_for_archives',
+    'Bucket name for archives',
+    'cf7_cloud_storage_bucket_name_for_archives_settings_field',
+    'cf7_cloud_storage',
+    'cf7_cloud_storage_settings_section'
+  );
+}
+
+function cf7_cloud_storage_admin_menu() {
+  add_options_page('Contact Form 7 Cloud Storage', 'Contact Form 7 Cloud Storage', 'manage_options', 'cf7_cloud_storage', 'cf7_cloud_storage_options_page');
+}
+
+function cf7_cloud_storage_options_page() {
+?>
+<div class="wrap">
+  <form action="options.php" method="POST">
+    <?php settings_fields('cf7_cloud_storage'); ?>
+    <?php do_settings_sections('cf7_cloud_storage'); ?>
+    <?php submit_button(); ?>
+  </form>
+</div>
+<?php
+}
+
+function cf7_cloud_storage_settings_section() {
+}
+
+function cf7_cloud_storage_service_account_key_settings_field() {
+?>
+  <textarea id="cf7_cloud_storage_service_account_key" name="cf7_cloud_storage_service_account_key"><?php echo esc_textarea(get_option('cf7_cloud_storage_service_account_key')); ?></textarea>
+<?php
+}
+
+function cf7_cloud_storage_bucket_name_for_uploads_settings_field() {
+  ?>
+    <input id="cf7_cloud_storage_bucket_name_for_uploads" name="cf7_cloud_storage_bucket_name_for_uploads" value="<?php form_option('cf7_cloud_storage_bucket_name_for_uploads'); ?>">
+  <?php
+}
+    
+function cf7_cloud_storage_bucket_name_for_archives_settings_field() {
+  ?>
+    <input id="cf7_cloud_storage_bucket_name_for_archives" name="cf7_cloud_storage_bucket_name_for_archives" value="<?php form_option('cf7_cloud_storage_bucket_name_for_archives'); ?>">
+  <?php
+}
+    
+function cf7_cloud_storage_ajax_get_signed_url() {
+  $storage = new StorageClient([
+    'keyFile' => json_decode(get_option('cf7_cloud_storage_service_account_key'), true)
+  ]);
+  $bucket = $storage->bucket(get_option('cf7_cloud_storage_bucket_name_for_uploads'));
+  $object = $bucket->object(uniqid() . '/' . $_POST['name']);
+ 
+  $url = $object->signedUrl(
+      new \DateTime('1 day'),
+      [
+          'method' => 'PUT',
+          'contentType' => $_POST['type'],
+          'version' => 'v4'
+      ]
+  );
+
+  echo $url;
+
+  wp_die();
+}
+
+function cf7_cloud_storage_wpcf7_before_send_mail($form) {
+  $submission = WPCF7_Submission::get_instance();
+ 
+  if ($submission) {
+    $data = $submission->get_posted_data();
+    if (empty($data)) {
+      return;
+    }
+
+    if (isset($data['cf7_cloud_storage_fields']) && is_array($data['cf7_cloud_storage_fields'])) {
+      $storage = new StorageClient([
+        'keyFile' => json_decode(get_option('cf7_cloud_storage_service_account_key'), true)
+      ]);
+
+      $uploadsBucket = $storage->bucket(get_option('cf7_cloud_storage_bucket_name_for_uploads'));
+      $archivesBucket = $storage->bucket(get_option('cf7_cloud_storage_bucket_name_for_archives'));
+
+      $mail = $form->prop('mail');
+
+      $directory = uniqid('', true);
+      foreach ($data['cf7_cloud_storage_fields'] as $name) {
+        if ($data[$name] !== '') {
+          $object = $uploadsBucket->object($data[$name]);
+          $object->copy($archivesBucket, [
+            'name' => $directory . '/' . basename($data[$name])
+          ]);
+
+          $mail = str_replace("[$name]", 'https://storage.googleapis.com/' . get_option('cf7_cloud_storage_bucket_name_for_archives') . '/' . $directory . '/' . basename($data[$name]), $mail);
+        }
+      }
+
+      $form->set_properties([ 'mail' => $mail ]);
+    }
+  }
+}
+
+function cf7_cloud_storage_wpcf7_enqueue_scripts() {
+  wp_enqueue_script('cf7-cloud-storage', plugin_dir_url(__FILE__) . 'js/cf7_cloud_storage.js', array(), null, true);
+  wp_enqueue_style('cf7-cloud-storage', plugin_dir_url(__FILE__) . 'css/cf7_cloud_storage.css', array(), null);
+
+  wp_add_inline_script('cf7-cloud-storage', 'var cf7_cloud_storage_ajax_url = '. json_encode(admin_url('admin-ajax.php')) . ';', 'before');
+}
+
+function cf7_cloud_storage_wpcf7_init() {
+  wpcf7_add_form_tag(array('file_cloud_storage'), 'cf7_cloud_storage_wpcf7_form_tag', array('name-attr' => true));
+}
+
+function cf7_cloud_storage_wpcf7_form_tag($tag) {
+  $id = 'cf7-cloud-storage-dropzone-' . $tag->name;
+  $accepts = $tag->get_option('accept', '');
+
+  return 
+    '<input type="hidden" name="cf7_cloud_storage_fields[]" value="' . htmlspecialchars($tag->name) . '">' .
+    '<div class="cf7-cloud-storage-dropzone" id=' . json_encode($id) . '></div>' .
+    '<script>document.addEventListener("DOMContentLoaded", function() {' .
+    '  cf7_cloud_storage_dropzone(' . json_encode($tag->name) . ', document.getElementById(' . json_encode($id) . '), ' . json_encode($accepts) . ');' .
+    '});</script>'
+  ;
+}
---
+++ js/cf7_cloud_storage.js
@@ -0,0 +1,165 @@
+var cf7_cloud_storage_ajax_nonce = 0;
+
+function cf7_cloud_storage_dropzone(name, dropzone, accepts) {
+    var isUploading = false;
+
+    var uploadFile = function() {
+        if (file.files.length > 1) {
+            return;
+        }
+
+        if (accepts && Array.isArray(accepts)) {
+            var accepted = false;
+            for (var extensionOrType of accepts) {
+                if (extensionOrType.startsWith('.')) {
+                    accepted |= file.files[0].name.endsWith(extensionOrType);
+                } else {
+                    accepted |= file.files[0].type == extensionOrType;
+                }
+            }
+
+            if (!accepted) {
+                message.classList.add('cf7-cloud-storage-dropzone-message-error');
+                message.innerText = 'File is not accepted.';
+
+                hidden.value = '';
+
+                return;
+            }
+        }
+
+        isUploading = true;
+
+        message.hidden = true;
+        progress.hidden = false;
+
+        var formData = new FormData();
+        formData.append('_wpnonce', cf7_cloud_storage_ajax_nonce++);
+        formData.append('action', 'get_signed_url');
+        formData.append('name', file.files[0].name);
+        formData.append('type', file.files[0].type);
+
+        var request = new XMLHttpRequest();
+        request.open('POST', cf7_cloud_storage_ajax_url, true);
+        request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    var uploadUrl = new URL(request.responseText);
+
+                    var uploadRequest = new XMLHttpRequest();
+
+                    uploadRequest.open('PUT', uploadUrl, true);
+                    uploadRequest.setRequestHeader('Content-Type', file.files[0].type);
+
+                    uploadRequest.upload.onprogress = function(e) {
+                        progress.value = e.loaded / e.total;
+                    };
+
+                    uploadRequest.onreadystatechange = function() {
+                        if (uploadRequest.readyState === 4) {
+                            isUploading = false;
+
+                            message.hidden = false;
+                            progress.hidden = true;
+
+                            if (uploadRequest.status === 200) {
+                                message.classList.add('cf7-cloud-storage-dropzone-message-success');
+                                message.innerText = file.files[0].name;
+
+                                hidden.value = uploadUrl.pathname.replace(/^\/[\w-]+\//, '');
+                            }
+                        }
+                    }
+
+                    uploadRequest.onerror = function() {
+                        isUploading = false;
+
+                        message.hidden = false;
+                        progress.hidden = true;
+                    };
+
+                    uploadRequest.send(file.files[0]);
+                } else {
+                    isUploading = false;
+
+                    message.hidden = false;
+                    progress.hidden = true;
+                }
+            }
+        };
+        request.onerror = function() {
+            isUploading = false;
+
+            message.hidden = false;
+            progress.hidden = true;
+        };
+        request.send(formData);
+    };
+
+    var hidden = document.createElement("input");
+    hidden.type = "hidden";
+    hidden.name = name;
+    dropzone.appendChild(hidden);
+
+    var file = document.createElement("input");
+    file.type = "file";
+    file.hidden = true;
+    dropzone.appendChild(file);
+
+    var message = document.createElement('div');
+    message.innerText = 'Drop a file here';
+    dropzone.appendChild(message);
+
+    var progress = document.createElement("progress");
+    progress.hidden = true;
+    progress.className = "cf7-cloud-storage-dropzone-progress"
+    dropzone.appendChild(progress);
+
+    file.addEventListener('change', uploadFile); 
+
+    dropzone.addEventListener('click', function (e) {
+        if (isUploading) {
+            return;
+        }
+
+        file.click();
+    });
+
+    dropzone.addEventListener('dragleave', function (e) {
+        dropzone.classList.remove('cf7-cloud-storage-dropzone-drag-hover');
+    });
+
+    dropzone.addEventListener('dragover', function (e) {
+        if (!e.dataTransfer.types.includes("Files")) {
+            return;
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (isUploading) {
+            return;
+        }
+
+        dropzone.classList.add('cf7-cloud-storage-dropzone-drag-hover');
+    });
+
+    dropzone.addEventListener('drop', function (e) {
+        if (!e.dataTransfer.types.includes("Files")) {
+            return;
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (isUploading) {
+            return;
+        }
+
+        dropzone.classList.remove('cf7-cloud-storage-dropzone-drag-hover');
+
+        file.files = e.dataTransfer.files;
+
+        uploadFile();
+    });
+}
